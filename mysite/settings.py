507ac91879934ec8b6dd3b9cc4e1d1effca2dc60
ShimamuraJunya <j.shimamura@gigei.jp>
2019-09-13 15:42:49 +0900

    Changed the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'd!7-50$xgu)4eud-fj9%#&8b9f_!g1hr+$gnb))^1&wlq+5+#7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['junyashimamura.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
